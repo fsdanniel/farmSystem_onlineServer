6f39bb8a3e5036a938e905a8847cb6e8e6aefc8b
eduardo-marca <eduardovmarca520@gmail.com>
2025-09-26 13:00:01 -0300

    Reorganize Modelo

---
+++ Grupo Modelo/include/backend.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <pqxx/pqxx>
+
+using namespace std;
+using namespace pqxx;
+
+void insereAdministrador(string Adm_Nome, string Adm_Senha, string Adm_Data, string Adm_Email);
+
+void removeAdministrador(string Adm_Nome);
+
+void insereFuncionario(string Fun_Nome, string Fun_Senha, string Fun_Data, string Fun_Email);
+
+void removeFuncionario(string Fun_Nome);
+
+void insereVeterinario(string Vet_Nome, string Vet_Senha, string Vet_Data, string Vet_Email);
+
+void removeVeterinario(string Vet_Nome);
+
+bool loginAdministrador(const string &Adm_Nome, const string &Adm_Senha);
+
+bool loginFuncionario(const string &Fun_Nome, const string &Fun_Senha);
+
+bool loginVeterinario(const string &Vet_Nome, const string &Vet_Senha);
--- Grupo Modelo/src/backend.cpp
+++ Grupo Modelo/src/backend.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <string>
-#include <pqxx/pqxx>
-using namespace std;
-using namespace pqxx;
+#include "../include/backend.hpp"
 
 void insereAdministrador(string Adm_Nome, string Adm_Senha, string Adm_Data, string Adm_Email) {
 
@@ -395,35 +391,4 @@ bool loginVeterinario(const string &Vet_Nome, const string &Vet_Senha) {
         return false;
 
     }
-}
-
-
-int main() {
-
-    insereAdministrador("Aeronautics Barata", "WLEF7YTE", "2001-11-01", "AeronauticsBarata@tuta.io");
-    insereAdministrador("Otávio Bundasseca", "dafg87yfpq", "1978-07-13", "otaviozinho123o@yahoo.com.br");
-    insereAdministrador("Ilegível Inilegível", "XQEW3OI8UR45FH", "1964-03-31", "inelegivelInelegivel@tw1tt3r.com.us");
-    removeAdministrador("Aeronautics Barata");
-    //a remoção de um administrador não apaga o registro;
-
-    insereFuncionario("Lança Perfume Rodometálico de Andrade", "defvcetrdf6584r", "2011-11-03", "lancadorzinho@outlook.com");
-    insereFuncionario("Maria Privada de Jesus", "987fv78d", "1999-04-19", "mariaprivada@gmail.com");
-    insereFuncionario("Bandeirante do Brasil Paulistano", "erfdókpfe894", "1950-07-26", "paulistanobrasil@yahoo.com");
-    removeFuncionario("Lança Perfume Rodometálico de Andrade");
-    //a remoção de um funcionário não apaga o registro;
-
-    insereVeterinario("Antonio Manso Pacífico de Oliveira Sossegado", "fedrewd23", "1871-02-28", "antonio@yahoo.com");
-    insereVeterinario("Dolores Fuertes de Barriga", "juyu77urtfg", "1977-01-16", "dolores@yahoo.com");
-    insereVeterinario("Baleira Dorra", "gtrf434r5ujyh", "1958-11-05", "baleira@yahoo.com");
-    removeVeterinario("Antonio Manso Pacífico de Oliveira Sossegado");
-    //a remoção de um veterinário não apaga o registro;
-
-    cout << endl;
-
-    cout << loginAdministrador("Aeronautics Barata", "WLEF7YTE") << endl;
-    cout << loginFuncionario("Maria Privada de Jesus", "987fv78d") << endl;
-    cout << loginVeterinario("Baleira Dorra", "gtrf434r5ujyh") << endl;
-
-    return 0;
-
 }

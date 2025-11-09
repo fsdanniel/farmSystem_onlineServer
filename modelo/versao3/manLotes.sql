16c33047bc1da4e2abd147330b43efc8c8d39db7
rovatsug <gustavorichtergustavo@gmail.com>
2025-11-08 23:07:41 -0300

    mensagem

--- modelo/versao3/manLotes.sql
+++ modelo/versao3/manLotes.sql
@@ -2,8 +2,11 @@
 
 INSERT INTO LOTES(lote_nome, lote_genetica, lote_quantidade, lote_dataCriacao) VALUES ($1, $2, $3, $4);
 
--- Filtrar por genetica:
+-- Filtrar por genetica/status:
+EXEC filtragemGeneticaStatus(gen, status);
 
--- Filtrar por status
+-- listagem {ID 	Nome 	Genética 	Qtd. Animais 	Data Criação 	Status}
+SELECT lote_id, lote_nome, lote_genetica, lote_quantidade, lote_dataCriacao, lote_status
+FROM lotes;
 
--- listagem {ID 	Nome 	Genética 	Qtd. Animais 	Data Criação 	Status 	Ações}
+
